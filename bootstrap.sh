b90d4363303a82b73a0a1554f46ecbb0fbfc01c6
Azul <mail@azulinho.com>
2015-11-29 21:19:25 +0000

    first commit

---
+++ bootstrap.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# http://mechanicalcat.net/richard/log/OpenStack/Easy_installation_of_a_new_stack_with_OpenStack_Ansible_Deployment__OSAD/Horizon_development_with_openstack_ansible
+
+# Update repository information and install git
+apt-get update && apt-get install -y git
+
+# Clone the OSA repository
+test -e /opt/openstack-ansible || git clone https://github.com/openstack/openstack-ansible /opt/openstack-ansible
+
+# Bootstrap the env
+cd /opt/openstack-ansible
+./scripts/bootstrap-aio.sh
+ 
+# Bootstrap ansible
+./scripts/bootstrap-ansible.sh
+
+# Set the internal address to the external
+sed -i "s/internal_lb_vip_address:.*/internal_lb_vip_address: \"{{ external_lb_vip_address }}\"/" \
+  /etc/openstack_deploy/openstack_user_config.yml​
+
+# disable unnecessary services
+
+sed -i "s/horizon_container: 2/horizon_container: 1/" /etc/openstack_deploy/openstack_user_config.yml 
+sed -i "s/galera_container: 3/galera_container: 1/" /etc/openstack_deploy/openstack_user_config.yml
+sed -i "s/rabbit_mq_container: 3/rabbit_mq_container: 1/" /etc/openstack_deploy/openstack_user_config.yml
+
+scripts/run-playbooks.sh
+
+pushd playbooks
+openstack-ansible os-tempest-install.yml
+popd
+
+grep admin_pass /etc/openstack_deploy/user_secrets.yml
+
+
